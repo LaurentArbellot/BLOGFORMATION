aa2e3de5414aca83741c06bdc6420d69dc2f3a75
jo <montana>
2023-06-21 11:09:33 +0200

    Navbar+ classe active +templates +route contact

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -5,14 +5,24 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-
+#[Route('/', name: 'app_main_')]
 class MainController extends AbstractController
 {
-    #[Route('/', name: 'app_main')]
+    #[Route('/', name: 'home')]
     public function home(): Response
     {
         return $this->render('main/home.html.twig', [
             'controller_name' => 'Home',
+        ]);
+    }
+
+
+
+    #[Route('/contact', name: 'contact')]
+    public function contact(): Response
+    {
+        return $this->render('main/home.html.twig', [
+            'controller_name' => 'Contact',
         ]);
     }
 }
