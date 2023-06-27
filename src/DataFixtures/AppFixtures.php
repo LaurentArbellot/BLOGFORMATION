93ed6030579587802d124743e193ed365bbf67a9
jo <montana>
2023-06-27 12:42:13 +0200

    PARAMETRAGE DE LA methode fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,14 +4,37 @@ namespace App\DataFixtures;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use App\Entity\Users;
+use App\Entity\Posts;
+use App\Entity\Comments;
 
 class AppFixtures extends Fixture
 {
-    public function load(ObjectManager $manager): void
+
+    private UserPasswordHasherInterface $hasher;
+    public function __construct(UserPasswordHasherInterface $hasher)
     {
+        $this->hasher = $hasher;
+    }
+    public function load(ObjectManager $manager): void
+    { 
+        // creation d'un compte admin
+        $admin = new Users();
+        $admin
+            ->setUsername('admin')
+            ->setEmail('a@a.fr')
+            ->setPassword( $this->hasher->hashPassword($admin, 'Madlib91!'))
+            ->setIsverified(TRUE)
+            ->setRoles(["ROLE_ADMIN"]);
+
+       
+
+        $manager->persist($admin);
+        $manager->flush();
         // $product = new Product();
         // $manager->persist($product);
 
-        $manager->flush();
+     
     }
 }
